5b4384c8caabc1cfaed6e664c64e70cb7e23fc48
jleo <jleo2525@live.com>
2016-09-03 15:07:41 -0400

    adding test files (no impl) with makefile

---
+++ tests/dlist.tests.c
@@ -0,0 +1,7 @@
+#include "../lib/slist.h"
+#include <criterion/criterion.h>
+
+Test(dlist_misc, failing)
+{
+	cr_assert(0);
+}
---
+++ tests/slist.tests.c
@@ -0,0 +1,7 @@
+#include "../lib/slist.h"
+#include <criterion/criterion.h>
+
+Test(misc, failing)
+{
+	cr_assert(0);
+}
